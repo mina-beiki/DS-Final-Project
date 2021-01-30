9744097ed542bc5cc179b033ff148bc68325b1f0
Mina Beiki <9831075@ceit.local>
2021-01-30 09:41:28 +0330

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.* ;
+
+public class Main {
+    public static void main(String[] args) {
+        int n , m ; //n=vertexes , m=edges
+        Scanner scanner = new Scanner(System.in);
+        n=scanner.nextInt();
+        m=scanner.nextInt();
+        //System.out.println("n="+n+"m="+m);
+        for(int i=0 ; i<n ; i++){
+
+        }
+    }
+}
